192985ecc535590b0c6433b3cb68e4dede5ea441
Anusha Chengi <anushachengi@gmail.com>
2018-11-24 13:47:10 +0530

    Created a new sample

---
+++ Sample 2/Sample 2/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Sample 2
+//
+//  Created by Anusha on 24/11/18.
+//  Copyright © 2018 Anusha. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
